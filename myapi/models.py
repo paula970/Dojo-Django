ce08d0a11833623e7528c6a07682879dde7fafd0
paula970 <andrea.maldonado@udea.edu.co>
2020-09-26 12:00:20 -0500

    version 1

---
+++ myapi/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Hero(models.Model):
+    name=models.CharField(max_length=60)
+    alias=models.CharField(max_length=60)
+
+    def _str_(self):
+        return self.name
+
